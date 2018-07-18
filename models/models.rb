795360a3f7d8d1e086840b1a8742c3461fe94c7b
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-18 17:40:25 +0000

    automatically backed up by learn

--- models/models.rb
+++ models/models.rb
@@ -5,7 +5,9 @@ def get_animal(animal,hat)
     elsif hat == "top hat"
       animal_img == "http://texastravesty.com/sites/default/files/field/image/Local%20dog%20wears%20hat%20looks%20great%201000.jpg"
     if hat = "bucket hat"
-      animal_img == ""
-    
+      animal_img == "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
+    end 
   elsif animal == "cat"
+    if hat == "party hat"
+      animal_img == 
   
