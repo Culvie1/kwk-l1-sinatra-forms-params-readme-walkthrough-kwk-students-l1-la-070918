5bad6cf9733a5320a13a3957b644487ea59c01e6
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-18 17:44:46 +0000

    automatically backed up by learn

--- models/models.rb
+++ models/models.rb
@@ -4,7 +4,7 @@ def get_animal(animal,hat)
       animal_img = "https://www.latestfreestuff.co.uk/wp-content/uploads/2015/04/dog-in-party-hat.jpg"
     elsif hat == "top hat"
       animal_img = "http://texastravesty.com/sites/default/files/field/image/Local%20dog%20wears%20hat%20looks%20great%201000.jpg"
-    if hat = "bucket hat"
+    if hat == "bucket hat"
       animal_img = "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
     end 
   elsif animal == "cat"
